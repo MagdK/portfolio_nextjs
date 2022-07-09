236a407d1572b5ac57385ba8c99e2f8bdc930191
MagdK <kovacsmagda@gmail.com>
2022-07-09 14:41:30 +0200

    Set up desktop with with variable

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,7 +3,9 @@ import styles from './Footer.module.scss';
 const Footer = () => {
     return (
         <footer className={styles.footer_container}>
-            <p>Made by Magda with <span>♥</span> and lots of 🍵.</p>
+            <div className={styles.footer_wrapper}>
+                <p>Made by Magda with <span>♥</span> and lots of 🍵.</p>
+            </div>
         </footer>        
     )
 }
