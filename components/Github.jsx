6bb95b119b8fa3da8e6621bc2994dee21fd7a31b
MagdK <kovacsmagda@gmail.com>
2022-07-09 14:44:30 +0200

    Remove github and linked icons as components

--- components/Github.jsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-
-const Github = () => {
-  return <Image src="/github.svg" alt="Github logo" href="https://github.com/MagdK" targer="_blank" width="24" height="24" />
-}
-
-export default Github;
--- components/Linkedin.jsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-
-const LinkedIn = () => {
-  return <Image src="/linkedin.svg" alt="LinkedIn logo" width="24" height="24" />
-}
-
-export default LinkedIn;
