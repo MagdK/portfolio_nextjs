f84235dbc818fd1232af42da587222db629dc27f
MagdK <kovacsmagda@gmail.com>
2022-07-09 17:03:05 +0200

    Make the layout responsive in  contact section (desktop & tablet, not yet for mobile)

--- components/contact.jsx
+++ components/contact.jsx
@@ -6,20 +6,22 @@ const Contact = () => {
         <section className={styles.contact_container} id="contact_section">
             <div className={styles.contact_wrapper}>
                 <h1>contact.</h1>
-                <p>
-                You have gotten this far? Amazing, lets talk.
-                </p>
-                <p>
-                I am available for Product Design and Frontend Development projects. 
-                Drop me a message and lets create something great together!
-                </p>
+                <div className={styles.contact_description}>
+                    <p>
+                    You have gotten this far? Amazing, lets talk.
+                    </p>
+                    <p>
+                    I am available for Product Design and Frontend Development projects. 
+                    Drop me a message and lets create something great together!
+                    </p>
+                </div>
                 <div className={styles.btn_container}>
-                <a href="https://www.linkedin.com/in/magda-kovacs/" target="_blank" rel="noreferrer">
-                    <PrimaryButton>Connect on LinkedIn</PrimaryButton>
-                </a>
-                <a href="mailto:kovacsmagda@gmail.com">
-                    <TertiaryButton>Send an email</TertiaryButton>
-                </a>
+                    <a href="https://www.linkedin.com/in/magda-kovacs/" target="_blank" rel="noreferrer" className={styles.linkedin_btn}>
+                        <PrimaryButton >Connect on LinkedIn</PrimaryButton>
+                    </a>
+                    <a href="mailto:kovacsmagda@gmail.com" className={styles.email_btn}>
+                        <TertiaryButton >Send an email</TertiaryButton>
+                    </a>
               </div>
             </div>
         </section>
