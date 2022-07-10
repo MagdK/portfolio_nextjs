60e6f9fe6e05ebfed385842be16b1222d27cd22d
MagdK <kovacsmagda@gmail.com>
2022-07-10 20:57:32 +0200

    On mobile, after menu click sidebar closes

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -4,53 +4,28 @@ import React, { useState, useEffect } from 'react';
 
 
 const Navigation = () => {
-  /*   const [show, setShow] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
+  const [showSidebar, setShowSidebar] = useState(false);
+  const closeSidebar = () => {setShowSidebar(false)};
 
-  const controlNavbar = () => {
-    if (typeof window !== 'undefined') { 
-      if (window.scrollY > lastScrollY) { // if scroll down hide the navbar
-        setShow(false); 
-      } else { // if scroll up show the navbar
-        setShow(true);  
-      }
-
-      // remember current page location to use in the next move
-      setLastScrollY(window.scrollY); 
-    }
-  };
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('scroll', controlNavbar);
-
-      // cleanup function
-      return () => {
-        window.removeEventListener('scroll', controlNavbar);
-      };
-    }
-  }, [lastScrollY]); */
-  
-
-    return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-              <a href="#top">
-                <h2 className={styles.logo_container}>m<span>.</span></h2>
-              </a>
-            </div>
-            <div className={styles.navigation}>
-              <input className={styles.toggle_menu} type="checkbox"/>
-              <div className={styles.hamburger}></div>
-              <ul className={styles.menu}>
-                <li><a href="#top">Home</a></li>
-                <li><a href="#about_section">About</a></li>
-                <li><a href="#work_section">Work</a></li>
-                <li><a href="#contact_section">Contact</a></li>
-              </ul>
-            </div>
-        </header>
-    )
+  return (
+      <header className={styles.header}>
+          <div className={styles.logo}>
+            <a href="#top">
+              <h2 className={styles.logo_container}>m<span>.</span></h2>
+            </a>
+          </div>
+          <div className={styles.navigation}>
+            <input className={styles.toggle_menu} type="checkbox" checked={showSidebar} onClick={() => {setShowSidebar(!showSidebar)}}/>
+            <div className={styles.hamburger}></div>
+            <ul className={styles.menu}>
+              <li><a href="#top" onClick={closeSidebar}>Home</a></li>
+              <li><a href="#about_section" onClick={closeSidebar}>About</a></li>
+              <li><a href="#work_section" onClick={closeSidebar}>Work</a></li>
+              <li><a href="#contact_section" onClick={closeSidebar}>Contact</a></li>
+            </ul>
+          </div>
+      </header>
+  )
 }
 
 export default Navigation;
