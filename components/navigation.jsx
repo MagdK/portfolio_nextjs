e2686e2977d9b1ac8daf797fc6d05ca388520bac
MagdK <kovacsmagda@gmail.com>
2022-07-10 18:43:36 +0200

    Set up burger meno for mobile

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -34,25 +34,23 @@ const Navigation = () => {
 
     return (
         <div className={styles.header_container}>
-            <div className={styles.nav_container}>
-                <a href="#top">
-                    <h2 className={styles.logo_container}>m<span>.</span></h2>
-                </a>
-                <ul className={styles.menu_container}>
-                    <li><a href="#top">Home</a></li>
-                    <li><a href="#about_section">About</a></li>
-                    <li><a href="#work_section">Work</a></li>
-                    <li><a href="#contact_section">Contact</a></li>
-                </ul>
-                {/* <div className={styles.social_icons_container}>
-                    <a href="https://www.linkedin.com/in/magda-kovacs/"  className={styles.social_icon} target="_blank" rel="noreferrer">
-                        <Image src="/linkedin.svg" alt="LinkedIn logo" width="24" height="24" className={styles.social_icon_linkedin}/>
-                    </a>
-                    <a href="https://github.com/MagdK" className={styles.social_icon} target="_blank" rel="noreferrer" >
-                        <Image src="/github.svg" alt="Github logo" href="https://github.com/MagdK" targer="_blank" width="24" height="24"/>
-                    </a>
-                </div> */}
-            </div>
+            <nav className={styles.nav_container}>
+              <input id="nav-toggle" type="checkbox"/>
+              <a href="#top">
+                <h2 className={styles.logo_container}>m<span>.</span></h2>
+              </a>
+              <ul className={styles.menu_container}>
+                <li><a href="#top">Home</a></li>
+                <li><a href="#about_section">About</a></li>
+                <li><a href="#work_section">Work</a></li>
+                <li><a href="#contact_section">Contact</a></li>
+              </ul>
+              <label className={styles.burger}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+              </label>
+            </nav>
         </div>
     )
 }
