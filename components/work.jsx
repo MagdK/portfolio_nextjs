3273d0cf8938136e3254fcfbc5807b0e4d48a4e7
MagdK <kovacsmagda@gmail.com>
2022-07-14 16:00:52 +0200

    Update fashionTrade description

--- components/work.jsx
+++ components/work.jsx
@@ -11,21 +11,13 @@ const Work = () => {
             <p>
             Below are some of my projects I have worked on throughout my years of design and development.
             </p>
-
-            <div className={styles.work_list_card}>
-              <h4>Title</h4>
-              <p>Year</p>
-              <p>Company</p>
-            </div>
-            
-
-    
-              
-                  <Link href="/details">
-                    <a>Go to details</a>
-                  </Link>
-            
-            
+            <Link href="/details">
+              <div className={styles.work_list_card}>
+                <div>
+                  <h4>Title</h4>
+                </div>
+              </div> 
+            </Link>
           </div>
         </section>
     )
