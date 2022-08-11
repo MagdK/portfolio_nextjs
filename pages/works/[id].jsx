7652a16e0ab0ff43cc5558e6f98036029c6708d1
MagdK <kovacsmagda@gmail.com>
2022-08-11 18:01:39 +0200

    Install remark and remark-html

--- pages/works/[id].jsx
+++ pages/works/[id].jsx
@@ -1,21 +1,38 @@
-import { useRouter } from 'next/router';
 import fs from 'fs';
-import path from 'path';
+// import glob from 'glob';
+import matter from 'gray-matter';
+import { remark } from 'remark';
+import html from 'remark-html';
 
-const Work = ({ title }) => {
-  const router = useRouter()
-  const { id } = router.query
+const Work = ({ frontmatter, content }) => {
 
-  return <p>Work: {id}, {title}</p>
+  return <p>Work: {frontmatter.toString()}, {content}</p>
 }
 
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join("works"))
+export async function getStaticProps(context) {
+  const slug = context.params.id;
+  const filePath = `works/${slug}/${slug}.md`;
+  const markdownWithMeta = fs.readFileSync(filePath, 'utf-8');
+
+  const {data:frontmatter, content} = matter(markdownWithMeta);
+
 
   return {
     props: {
-      posts: "The Posts"
+      frontmatter, content
     }
+  }
+}
+
+async function markdownToHtml(markdown) {
+  const result = await remark().use(html).process(markdown)
+  return result.toString()
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: 'begame-design-system' } }],
+    fallback: false, // can also be true or 'blocking'
   }
 }
 
