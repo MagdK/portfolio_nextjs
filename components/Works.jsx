7fefad0bf0584b5165b0fdf1250afad6352d4316
MagdK <kovacsmagda@gmail.com>
2022-08-17 16:24:18 +0200

    Move project mapping to Works component

---
+++ components/Works.jsx
@@ -0,0 +1,84 @@
+import styles from './Works.module.scss';
+import Link from 'next/link';
+// import Image from 'next/image';
+
+const Works = ({ works }) => {
+    console.log(works)
+    return (
+        <div className={styles.card_list}>
+            {works.map((work) => {
+
+                return(
+                    <div key={work.slug}>
+                        <h2>{work.frontmatter.title}</h2>
+                        <h4>{work.frontmatter.date}</h4>
+                        <Link href={`/works/${work.slug}`}>
+                            <a>Details</a>
+                        </Link>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Works;
+
+
+// const Work = () => {
+//     return (
+//         <section className={styles.work_container} id="work_section">
+//           <div className={styles.work_wrapper}>
+//             <h1>work.</h1>
+//             <p>
+//             Below are some of my projects I have worked on throughout my years of design and development.
+//             </p>
+//             <div className={styles.work_list_wrapper}>
+              
+//               <Link href="/details">
+//                 <div className={styles.work_list_card}>
+                  
+//                     <Image src="/images/works/begame-transaction-history.png" alt="Transaction Image" width="400" height="400" className={styles.work_image}></Image>
+
+//                   <div className={styles.work_list_card_description}>
+//                     <p>Bingocams transactions</p>
+//                     <p className={styles.tag}>Development and design</p>
+//                   </div>
+//                 </div> 
+//               </Link>
+
+//               <Link href="/details">
+//                 <div className={styles.work_list_card}>
+//                   <Image src="/images/works/begame-profile-picture-upload.png" alt="Transaction Image" width="400" height="400" className={styles.work_image}></Image>
+//                   <div className={styles.work_list_card_description}>
+//                     <p>Bingocams profile picture upload</p>
+//                     <p className={styles.tag}>Design</p>
+//                   </div>
+//                 </div> 
+//               </Link>
+
+//               <Link href="/details">
+//                 <div className={styles.work_list_card}>
+//                   <Image src="/images/works/begame-design-system.png" alt="Transaction Image" width="400" height="400" className={styles.work_image}></Image>
+//                   <div className={styles.work_list_card_description}>
+//                     <p>Bingocams design system</p>
+//                     <p className={styles.tag}>Design</p>
+//                   </div>
+//                 </div> 
+//               </Link>
+
+//             </div>
+//             <div className={styles.btn_container}>
+//               <PrimaryButton 
+//                   href="/works" 
+//                   rel="noreferrer" 
+//               >
+//                   Show all
+//               </PrimaryButton>
+//             </div>
+//           </div>
+//         </section>
+//     )
+// }
+
+// export default Work;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
+import fs from 'fs';
+import glob from 'glob';
+import path from 'path';
+import matter from 'gray-matter';
+
 import "swiper/css/bundle";
 import Head from 'next/head';
 
 import Navigation from "../components/Navigation";
 import Home from '../components/Home';
 import About from '../components/About';
-import Work from '../components/Work';
+import Works from '../components/Works';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
 
-export default function Main() {
+export default function Main({works}) {
   return (
     <div >
       <Head>
@@ -19,10 +24,38 @@ export default function Main() {
       <main >
         <Home />
         <About />
-        <Work />
+        <Works works={works} />
         <Contact />
       </main>
       <Footer />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  // Get files from the work directory
+  const files = glob.sync(path.join("public/works/*/*.md"))
+
+  const works = files.map(filename => {
+      // Create slug - https://magdadot.com/works/<slug>
+      const slug = path.basename(filename, '.md')
+
+      // Get frontmatter
+      const markdownWithMeta = fs.readFileSync(filename, 'utf-8')
+
+      const {data:frontmatter} = matter(markdownWithMeta)
+
+      return {
+          slug, 
+          frontmatter
+      }
+  })
+
+
+  // Get slug and frontmatter from works
+  return {
+      props: {
+      works
+      }
+  }
 }
