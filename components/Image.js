4573880c3a8057d9fc81f679f09d9b700cf75d1a
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-08-24 21:49:01 +0200

    Add image path configuration
    GitHub Pages, by default, served under a base path /<repo-name>.
    Images are also served under that base path, so the image loader needs
    to be able to generate the correct image path.
    
    The configuration file is loaded into the process.env.__NEXT_IMAGE_OPTS
    value. It's a every value under the `images` key from the
    nextjs.config.js file.
    
    https://github.com/vercel/next.js/blob/b757db43787bc4bbaab3d2af315c879fefd65d25/packages/next/client/image.tsx

--- components/Image.js
+++ components/Image.js
@@ -1,8 +1,14 @@
 import NextImage from "next/image";
 
+function normalizeSrc(src) {
+  return src[0] === '/' ? src.slice(1) : src
+}
+
 // opt-out of image optimization, no-op
 const customLoader = ({ src }) => {
-  return src
+  const config = process.env.__NEXT_IMAGE_OPTS
+  // https://github.com/vercel/next.js/blob/canary/packages/next/client/image.tsx#L27
+  return `${config.path}${normalizeSrc(src)}`
 }
 
 export default function Image(props) {
--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/portfolio_nextjs' : '',
+  assetPrefix: isProd ? '/portfolio_nextjs/' : '',
   basePath: isProd ? '/portfolio_nextjs' : '',
-  images: { loader: 'custom' }
+  images: {
+    loader: 'custom',
+    path: isProd ? '/portfolio_nextjs/' : ''
+  }
 }
 
 module.exports = nextConfig
