0e97a526b9e4291082f35fc8f8e6e98965333139
Magda Kovács <kovacsmagda@gmail.com>
2022-08-24 18:34:20 +0200

    Set up basePath so links work on github pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/portfolio_nextjs/' : '',
+  assetPrefix: isProd ? '/portfolio_nextjs' : '',
+  basePath: isProd ? '/portfolio_nextjs' : '',
   images: { loader: 'custom' }
 }
 
