2edd71efc05889bf9556eb8494df98e19c98c2bb
MagdK <kovacsmagda@gmail.com>
2022-08-25 09:11:03 +0200

    Revert "add image path configuration" commit

--- components/Image.js
+++ components/Image.js
@@ -1,14 +1,8 @@
 import NextImage from "next/image";
 
-function normalizeSrc(src) {
-  return src[0] === '/' ? src.slice(1) : src
-}
-
 // opt-out of image optimization, no-op
 const customLoader = ({ src }) => {
-  const config = process.env.__NEXT_IMAGE_OPTS
-  // https://github.com/vercel/next.js/blob/canary/packages/next/client/image.tsx#L27
-  return `${config.path}${normalizeSrc(src)}`
+   return src
 }
 
 export default function Image(props) {
