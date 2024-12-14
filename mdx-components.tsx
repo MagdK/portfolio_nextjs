9e3785bb740aa2e1fc1618963890d4ab75659750
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2024-12-14 15:48:50 +0100

    Use mdx-components.tsx instead of jsx

---
+++ mdx-components.tsx
@@ -0,0 +1,17 @@
+import type { MDXComponents } from 'mdx/types'
+import Image, { ImageProps } from 'next/image'
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    img: (props) => {
+      <Image
+        width="100%"
+        height="100%"
+        layout="responsive"
+        objectFit="contain"
+        {...(props as ImageProps)}
+      />
+    },
+    ...components
+  }
+}
