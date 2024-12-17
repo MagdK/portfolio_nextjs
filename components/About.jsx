e15f6545946c02f19541cacd9b735c03e23069a2
Magda Kovács <kovacsmagda@gmail.com>
2024-12-17 16:44:23 +0100

    Copy update

--- components/About.jsx
+++ components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
         Everything I’ve done has been in pursuit of <b className={styles.highlight}>understanding people</b> more deeply and <b className={styles.highlight}>solving problems</b>.
         </p>
         <p>
-        As a <b className={styles.highlight}>designer and developer</b>, I’m interested in how to create experiences that are <b className={styles.highlight}>impactful</b>, <b className={styles.highlight}>intuitive</b>, and perhaps, even a bit <b className={styles.highlight}>delightful</b>.
+        As a <b className={styles.highlight}>designer</b>, I’m interested in how to create experiences that are <b className={styles.highlight}>impactful, intuitive</b>, and perhaps, even a bit <b className={styles.highlight}>delightful</b>.
         </p>
         <p>
         When I’m not working, you’ll find me sipping tea and exploring the latest design trends, always eager to learn more about the human mind and body.
