a8a4e3272f7a613203d1eec5ded8952c3c5303be
Magda Kovács <kovacsmagda@gmail.com>
2025-01-07 16:44:08 +0100

    Set up max width for top nav

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -11,20 +11,22 @@ const Navigation = () => {
 
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <Link href="/#top">
-          <h2 className={styles.logo_container}>m<span>.</span></h2>
-        </Link>
-      </div>
-      <div className={styles.navigation}>
-        <input className={styles.toggle_menu} type="checkbox" checked={showSidebar} onChange={() => { setShowSidebar(!showSidebar) }} />
-        <div className={styles.hamburger}></div>
-        <ul className={styles.menu}>
-          <li><Link href="/#top" onClick={closeSidebar}>Home</Link></li>
-          <li><Link href="/#about_section" onClick={closeSidebar}>About</Link></li>
-          <li><Link href="/#work_section" onClick={closeSidebar}>Work</Link></li>
-          <li><Link href="/#contact_section" onClick={closeSidebar}>Contact</Link></li>
-        </ul>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <Link href="/#top">
+            <h2 className={styles.logo_container}>m<span>.</span></h2>
+          </Link>
+        </div>
+        <div className={styles.navigation}>
+          <input className={styles.toggle_menu} type="checkbox" checked={showSidebar} onChange={() => { setShowSidebar(!showSidebar) }} />
+          <div className={styles.hamburger}></div>
+          <ul className={styles.menu}>
+            <li><Link href="/#top" onClick={closeSidebar}>Home</Link></li>
+            <li><Link href="/#about_section" onClick={closeSidebar}>About</Link></li>
+            <li><Link href="/#work_section" onClick={closeSidebar}>Work</Link></li>
+            <li><Link href="/#contact_section" onClick={closeSidebar}>Contact</Link></li>
+          </ul>
+        </div>
       </div>
     </header>
   )
