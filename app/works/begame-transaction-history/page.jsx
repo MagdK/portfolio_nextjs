99d03a2b405002d823a1e84b779ca21b91dd1acf
Magda Kovács <kovacsmagda@gmail.com>
2025-03-15 16:01:04 +0100

    Update transactions case study images, copy

--- app/works/begame-transaction-history/page.jsx
+++ app/works/begame-transaction-history/page.jsx
@@ -10,7 +10,6 @@ import img4 from './Transaction_04.png'
 import img5 from './Transaction_05.png'
 import img6 from './Transaction_06.png'
 import img7 from './Transaction_07.png'
-import img8 from './Transaction_08.png'
 
 // HTML meta tags
 export const metadata = {
@@ -24,7 +23,7 @@ const Page = async ({ params }) => {
             <Link href="/#work_section" className={styles.back_link}>Back to works</Link>
             <h1>{metadata.title}</h1>
             <div className={styles.work_description}>
-                <Image src={img1} />
+                <Image src={img1} alt="Results"/>
 
                 <h2 id="impact">Impact</h2>
                 <ul>
@@ -43,33 +42,42 @@ const Page = async ({ params }) => {
                     transaction processes themselves were functional, a lack of clear
                     information led to user confusion, increased customer support inquiries,
                     and, potentially, member loss.</p>
-                <h2 id="role">Role</h2>
-                <ul>
-                    <li>User research &amp; analysis</li>
-                    <li>UX and UI redesign</li>
-                    <li>Component creation</li>
-                    <li>Usability testing</li>
-                </ul>
-                <h2 id="timeline">Timeline</h2>
-                <ul>
-                    <li>3X2 weeks sprints</li>
-                </ul>
-                <h2 id="tools">Tools</h2>
-                <ul>
-                    <li>Sketch</li>
-                    <li>Zeplin</li>
-                    <li>Abstract</li>
-                    <li>Whimsical</li>
-                    <li>Usertesting.com</li>
-                    <li>BootstrapVue</li>
-                    <li>Material Icons</li>
-                    <li>Jira</li>
-                </ul>
+
+                <div className={styles.table_container}>
+                    <div className={styles.column}>
+                    <h2 id="tools">Tools</h2>
+                    <ul>
+                        <li>Sketch</li>
+                        <li>Zeplin</li>
+                        <li>Abstract</li>
+                        <li>Whimsical</li>
+                        <li>Usertesting.com</li>
+                        <li>BootstrapVue</li>
+                        <li>Material Icons</li>
+                        <li>Jira</li>
+                    </ul> 
+                    </div> 
+                    <div className={styles.column}>
+                        <h2 id="role">Role</h2>
+                        <ul>
+                            <li>User research &amp; analysis</li>
+                            <li>UX and UI redesign</li>
+                            <li>Component creation</li>
+                            <li>Usability testing</li>
+                        </ul>
+                        <h2 id="timeline">Timeline</h2>
+                        <ul>
+                            <li>3X2 weeks sprints</li>
+                        </ul>  
+                    </div>
+                </div>    
+                
                 <h2 id="project-overview">Project overview</h2>
                 <p>Confusing transaction processes led to frequent support requests.
                     This project focused on deposits and withdrawals, improved transaction
                     flows, and enhanced clarity of transaction history, reducing support
                     needs and boosting user satisfaction.</p>
+
                 <h2 id="problem-discovery">Problem discovery</h2>
                 <ul>
                     <li><p>Reviewed insights from the customer success team to understand
@@ -82,52 +90,71 @@ const Page = async ({ params }) => {
                 <Image src={img2}
                     title="Preview of transactions before the redesign"
                     alt="Before state of the transaction overview" />
-                <h2 id="key-findings">Key findings</h2>
-                <p>What do users want:</p>
-                <Image src={img3}
-                    title="List of users&#39; needs" alt="What do users want" />
+
+                <h2 id="key-findings">Key findings - users&#39; pain points</h2>
+                <ul>
+                    <li><p>Hard to access transaction overview.</p></li>
+                    <li><p>Transactions lack clear differentiation by type.</p></li>
+                    <li><p>Some of the transaction screens lack a branded experience resulting the users feeling insecure.</p></li>
+                    <li><p>Users have no way to cancel transactions unless contacting the support team.</p></li>
+                    <li><p>Transaction statuses are unclear.</p></li>
+                    <li><p>Poor colour contrast affects usability.</p></li>
+                </ul>
+
                 <h2 id="solution">Solution</h2>
                 <p>To address these challenges, I proposed a redesign of the
                     transactions page. The updated design was intended to provide a more
                     organized, accessible, and informative transaction experience that
                     empowered users to track their activity confidently and
                     independently.</p>
+
                 <h2 id="wireframes">Wireframes</h2>
                 <p>I created wireframes to outline key layout variations for the
                     transaction overview page and gathered user feedback on the
                     concepts.</p>
-                <Image src={img4} title="Concepts of new transaction" alt="Wiewframe" />
+                <Image src={img3} title="Concepts of new transaction" alt="Wiewframe" />
+
                 <h2 id="high-fidelity-design">High-fidelity design</h2>
                 <p>Once the direction was decided, I developed high-fidelity prototypes
                     that emphasized transaction status and clear labels, covering all
                     responsive breakpoints for a seamless experience across screen
                     sizes.</p>
-                <Image src={img6}
-                    title="Preview of all breakpoints and how cards were set up"
-                    alt="Breakpoints" />
+                    <div className={styles.image_container} style={{ marginTop: '2em' }}>
+                        <Mp4Video
+                            src="/works/begame-transaction-history-withdrawal_flow_02.mp4"
+                            width="100%"
+                            height="100%"
+                            layout="responsive"
+                            objectFit="contain"
+                        />
+                    </div>
+                
                 <h2 id="error-handeling-and-empty-states">Error handeling and empty
                     states</h2>
-                <p>I designed mockups for errors, empty states, and edge cases,
+                <p>I designed mockups for errors, empty states, and other edge cases,
                     collaborating closely with the development team.</p>
-                <Image src={img7}
+                <Image src={img6}
                     title="Preview of all errors that can happen during deposit and withdrawal"
                     alt="Unhappy flows" />
+
                 <h2 id="usability-testing">Usability testing</h2>
                 <p>I set up usability tests on UserTesting.com to gather feedback from
                     users and refine the high-fidelity prototypes. The most important
                     insights from were: - Allowing users to save payment methods saves time
                     and boosts satisfaction. - Toast messages offer sufficient feedback,
                     while dialog boxes are distracting and unnecessary.</p>
-                <Image src={img8}
+                <Image src={img7}
                     title="Being able to see the status of my transactions gives me such peace of mind, especially compared to before. Depositing and withdrawing is now a breeze!"
                     alt="Feedback from user" />
+
                 <h2 id="design-system">Design system</h2>
                 <p>To ensure consistency, I designed the transaction card components for
                     the shared library and created detailed guidelines to support
                     development and QA teams.</p>
-                <Image src={img5}
+                <Image src={img4}
                     title="Transaction cards in component library"
                     alt="Transaction history component" />
+
                 <h2 id="but-ux-is-never-done">But UX is never done…</h2>
                 <p>This project was completed four years ago, and looking back, here are
                     a few areas I’d love to improve today:</p>
@@ -142,16 +169,6 @@ const Page = async ({ params }) => {
                     <li><p>Enhance assisted onboarding to help all users quickly understand
                         the updated transaction processes.</p></li>
                 </ul>
-            </div>
-
-            <div className={styles.image_container} style={{ marginTop: '2em' }}>
-                <Mp4Video
-                    src="/works/begame-transaction-history-withdrawal_flow_02.mp4"
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="contain"
-                />
             </div>
         </div>
     )
