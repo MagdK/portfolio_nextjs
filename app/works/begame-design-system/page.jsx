faa4cf29de050c89cffc58eec0620086d18c132d
Magda Kovács <kovacsmagda@gmail.com>
2025-03-30 19:05:35 +0200

    Update design system case study

--- app/works/begame-design-system/page.jsx
+++ app/works/begame-design-system/page.jsx
@@ -4,8 +4,9 @@ import Image from '@/components/Image';
 import Link from 'next/link';
 import designSystem1 from './Design_system_01.png'
 import designSystem2 from './Design_system_02.png'
-import designSystem3 from './Design system_3.png'
-import designSystem4 from './Design system_4.png'
+import designSystem3 from './Design_system_03.png'
+import designSystem4 from './Design_system_04.png'
+import designSystem5 from './Design_system_05.png'
 
 // HTML meta tags
 export const metadata = {
@@ -21,93 +22,70 @@ const Page = async ({ params }) => {
 
       <Image src={designSystem1} alt="Sneak a peak"/>
       <div className={styles.work_description}>
-        {/* <h2>Bingocams</h2>
-        <p>Bingocams is an online bingo platform, primarily serving the UK, that brings people together through fun, interactive gameplay while fostering a strong sense of community.</p> */}
         <h2 id="impact">Impact</h2>
         <p>The introduction of the design system had a notable impact on both
-          the product and team workflows:</p>
+          the product and team workflows:
+        </p>
         <ul>
-          <li><p>Consistency and cohesion: The platform’s visual consistency
-            improved significantly, providing users with a seamless and intuitive
-            interface that aligned with Bingocams’ brand identity.</p></li>
-          <li><p>Efficiency and productivity: With a shared library of components
-            and design guidelines, the development team was able to implement new
-            features more quickly and with fewer iterations, reducing production
-            time and costs.</p></li>
-          <li><p>Scalability: As the product evolved, the design system allowed
-            for smooth scaling, enabling new features to integrate seamlessly into
-            the existing UI.</p></li>
-          <li><p>User satisfaction: The consistency and intuitiveness of the
-            updated UI led to positive feedback from users, enhancing their overall
-            experience on the platform.</p></li>
+          <li><p><b className={styles.emphasis}>Stronger collaboration:</b> By fostering open communication about teams&#39; needs, the system became a shared language that streamlined cross-team efforts.</p></li>
+          <li><p><b className={styles.emphasis}>Speed & efficiency:</b> A unified library of components reduced redundant work, enabling faster iteration and delivery while cutting production costs.</p></li>
+          <li><p><b className={styles.emphasis}>Scalability & flexibility:</b> As the product evolved, the system ensured consistency while allowing for effortless expansion and adaptation.</p></li>
+          <li><p><b className={styles.emphasis}>User impact:</b> A more cohesive and intuitive UI led to improved user satisfaction and engagement, validated through positive feedback.</p></li>
         </ul>
-        <h2 id="problem">Problem</h2>
-        <p>Bingocams.co.uk, a community-focused bingo platform, was undergoing a
-          UI refresh to improve the user experience. However, the lack of a
-          structured component library created inefficiencies in the design and
-          development process, often resulting in an inconsistent user experience
-          and requiring repetitive work for common UI elements. This inconsistency
-          affected both the platform’s usability and the speed of
-          implementation.</p>
 
-        <div className={styles.table_container}>
-          <div className={styles.column}>
-            <h2 id="tools">Deliverables</h2>
-              <ul>
-                <li>Design system</li>
-                <li>Token library</li>
-                <li>Component library</li>
-                <li>Design guidelines and documentation</li>
-                <li>Product design</li>
-              </ul> 
-              </div> 
-                <div className={styles.column}>
-                  <h2 id="role">Expertise</h2>
-                  <ul>
-                    <li>Design systems</li>
-                    <li>Design tokens</li>
-                    <li>Product design</li>
-                  </ul>
-                    <h2 id="timeline">Timeline</h2>
-                    <ul>
-                      <li>4X2 weeks sprints + continuous maintenance</li>
-                    </ul>  
-                  </div>
-          </div>  
+        <h2 id="project-overview">Problem statement</h2>
+        <p>
+        Bingocams, a UK-based online gaming platform, lacked a structured design system, making its much-needed UI update challenging and inconsistent. To support future growth and a cohesive user experience, I led the creation of a scalable, adaptable design system to ensure consistency, enhance collaboration, and streamline the UI refresh.
+        </p>        
 
-        <h2 id="project-overview">The challenge</h2>
-        <p>
-          Our goal was to create an adaptable design system that is easy to manage, and can support the expansion of the company&#39;s gaming brands.
-        </p>
-        <p>
-          To build a lasting design system, a solid foundation is essential.
-        </p>
-        <p>
-          Building components is the easy part, the true success of design systems—and their impact on company growth—lies in a well-structured token architecture, robust sub-systems, cohesive core styles, and strong team collaboration.
-        </p>
+        <div className={styles.table2_container}>
+          <div className={styles.column2}>
+          <Image src={designSystem2} alt="Team collaboration"/>
+          </div>
+          <div className={styles.column2}>
+            <h2 id="process">Healthy collaboration</h2>
+            <p>While teams often prioritize technical needs over cross-collaboration, one of the most challenging parts was finding balance. I focused on fostering a culture of open communication, actively listening to feedback, and incorporating it. This way, we created a system that worked for everyone.
+            </p>
+          </div>
+        </div>
 
-        <Image src={designSystem2} alt="Team collaboration"/>
+        <div className={styles.table2_container}>
+          <div className={styles.column2}>
+            <Image src={designSystem3} alt="Token architecture - two tiered setup"/>
+          </div>
+          <div className={styles.column2}>
+            <h2 id="process">Design token architecture</h2>
+            <p>Design tokens define a company’s visual style and make it easy to adapt for new brands, saving time and effort. This setup was built with engineers to ensure alignment with the code, enabling seamless collaboration through a shared language.</p>
+          </div>
+        </div>
 
-        <h2 id="process">Healthy collaboration</h2>
-        <p>
-        While design system development often prioritises technical needs over cross-team collaboration, finding a balance is crucial. By fostering a culture of open communication, adapting the system to team needs, and actively listening to feedback while incorporating it, we can create a system that truly works for everyone.
-        </p>
+        <div className={styles.table2_container}>
+          <div className={styles.column2}>
+            <Image src={designSystem4} alt="T-shirt sized buttons"/>
+          </div>
+          <div className={styles.column2}>
+            <h2 id="process">Sub-system</h2>
+            <p>I used t-shirt sizing to keep components consistent and create a clear hierarchy. It helped maintain visual balance, sped up decision-making, and made collaboration between designers and developers easier.</p>
+          </div>
+        </div>
+
+        <div className={styles.table2_container}>
+          <div className={styles.column2}>
+            <Image src={designSystem5} alt="T-shirt sized buttons"/>
+          </div>
+          <div className={styles.column2}>
+            <h2 id="process">Setting up components</h2>
+            <p>I used the atomic design approach for consistency. Small, reusable components ensured a unified design language and reduced redundancy across the system. This approach also helped the team work more efficiently and allowed for scalability.</p>
+          </div>
+        </div>
+
+        <h2 id="process">Documentation</h2>
+        <p>Well-written documentation is key for ensuring smooth cross-team collaboration and onboarding new team members. To achieve this, we created a single source of truth where everyone could easily learn how the system works.</p>
+
         <h2 id="results">Results</h2>
-        <p>The design system became a pivotal tool in ensuring a unified user
-          experience across Bingocams.co.uk. Thanks to the structured
-          collaboration, the platform’s UI consistency improved, resulting in a
-          better product for users and a more efficient development cycle for the
-          team. This project not only addressed the immediate needs of the UI
-          refresh but also set a scalable foundation for future growth and design
-          innovation.</p>
+        <p>The design system played a key role in creating a consistent user experience on Bingocams.co.uk. It improved UI consistency, streamlined development, and set the stage for future growth and innovation.</p>
+      
       </div>
-
-      {/* <div className={styles.image_container}>
-        <Image src={designSystem4} />
-        <Image src={designSystem3} />
-        <Image src={designSystem2} />
-        <Image src={designSystem1} />
-      </div> */}
     </div>
   )
 }
